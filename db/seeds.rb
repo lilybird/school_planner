20bab224b9cbb212905131a883e80d5e6c7d272d
Bart Spencer <azspencer@Bart-Spencers-MacBook-Pro.local>
2013-08-08 11:01:00 -0500

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-dates_hash = [ '2013-8-1', '2013-8-2', '2013-8-3', '2013-8-4', '2013-8-5', '2013-8-6', '2013-8-7', '2013-8-8', '2013-8-9', '2013-8-10', '2013-8-11', '2013-8-12', '2013-8-13']
-
 Day.destroy_all
-dates_hash.each do |date|
-	d = Day.new
-	d.day = date
-	d.save
-end
+
+d1 = Day.new
+d1.day = '2013-8-5'
+d1.save
+
+d2 = Day.new
+d2.day = '2013-8-6'
+d2.save
+
+d3 = Day.new
+d3.day = '2013-8-7'
+d3.save
+
+d4 = Day.new
+d4.day = '2013-8-8'
+d4.save
+
+d5 = Day.new
+d5.day = '2013-8-9'
+d5.save
+
+d6 = Day.new
+d6.day = '2013-8-10'
+d6.save
+
+d7 = Day.new
+d7.day = '2013-8-11'
+d7.save
 
 groups_hash = [ 'Tennis', 'Spell Bowl', 'Student Council', 'Ballet']
 
@@ -32,3 +53,26 @@ courses_hash.each do |course|
 	c.name = course
 	c.save
 end
+
+Homework.destroy_all
+h = Homework.new
+h.assignment = 'Key Terms Chapters 1-10'
+h.day_id = d1[id]
+h.save
+
+Practice.destroy_all
+p = Practice.new
+p = d2[id]
+p.save
+
+Project.destroy_all
+pr = Project.new
+pr.name = 'test project'
+pr.day_id = d3[id]
+pr.save
+
+Test.destroy_all
+t = Test.new
+t.subject = 'test subject'
+t.day_id = d4[id]
+t.save
